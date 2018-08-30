fa6b35f7490cce960fe34211ca70f5e9009e7914
ionut-j-dumitrascu <ionut.j.dumitrascu@gmail.com>
2018-08-30 12:07:49 +0300

    initial commit

---
+++ Projects/c5_embedded101/c5_embedded101/include/features.h
@@ -0,0 +1,12 @@
+/*
+ * features.h
+ *
+ *  Created on: 6 Aug 2018
+ *      Author: User
+ */
+
+#ifndef FEATURES_H_
+#define FEATURES_H_
+
+
+#endif /* FEATURES_H_ */
---
+++ Projects/c5_embedded101/c5_embedded101/include/functions.h
@@ -0,0 +1,18 @@
+/*
+ * functions.h
+ *
+ *  Created on: 6 Aug 2018
+ *      Author: User
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+
+void WDOG_disable (void);
+void SOSC_init_8MHz(void);
+void SPLL_init_160MHz(void);
+void NormalRUNmode_80MHz (void);
+
+
+#endif /* FUNCTIONS_H_ */
